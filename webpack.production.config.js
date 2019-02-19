f56ea464cc7cc9edbcbae81deaf2b2eb9226017d
liyu28 <liyu28@meituan.com>
2019-02-19 12:07:18 +0800

    环境完善

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,14 +1,12 @@
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     mode: 'production',
     devtool: 'cheap-module-source-map',
-    entry: __dirname + '/src/index.js', //唯一入口文件
+    entry: __dirname + '/index.js', //唯一入口文件
     output: {
-        path: __dirname + '/release/', //打包后的文件存放的地方
-        filename: 'js/[name]-[hash].js', //打包后输出文件的文件名
-        publicPath: '' //js文件引入目录
+        path: __dirname + '/dist/', //打包后的文件存放的地方
+        filename: 'dateformat-js.min.js' //打包后输出文件的文件名
     },
 
     module: {
@@ -21,5 +19,12 @@ module.exports = {
                 exclude: /node_modules/
             }
         ]
-    }
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(['dist'], {
+            root: __dirname,
+            dry: false // 启用删除文件
+        })
+    ]
 };
